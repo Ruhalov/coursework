7feb3ba54cb4680b10cc84ed3cc61b043369be10
Hellpower21 <40489058+Hellpower21@users.noreply.github.com>
2021-03-31 16:12:10 +0300

    Hello world on SFML
    Пустое окно 800х600 залитое цветом RGB(242.232.201)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
+#include "SFML/Graphics.hpp"
+
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Coursework!");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color(242, 232, 201));
+        window.display();
+    }
 	return 0;
 }
